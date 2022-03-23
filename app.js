b061b64b80a6fbad5bba702df17e7d9f566bbd2f
Daniel Yatali <danielyatali902@gmail.com>
2022-03-22 23:13:09 -0400

    save messages on the server

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-const express = require("express");
-const socketIo = require("socket.io");
-const http = require("http");
-const path = require("path");
+import fetch from "node-fetch";
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { join } from "path";
+import path from "path";
 const PORT = process.env.PORT || 5000;
 const app = express();
-const server = http.createServer(app);
+const server = createServer(app);
 
-const io = socketIo(server, {
+const io = new Server(server, {
   cors: {
     origin: "*",
   },
 });
 
 //in case server and client run on different urls
+const __dirname = path.resolve();
+app.use(express.static(join(__dirname, "public")));
+// app.get("/", (req, res) => {
+//   async function fetchAsync() {
+//     let response = await fetch(
+//       "https://hpofficepaper-database-chatapp.herokuapp.com/api/users"
+//     );
+//     let data = await response.json();
+//     console.log(data);
+//   }
 
-app.use(express.static(path.join(__dirname, "public")));
+//   fetchAsync();
+//   res.send("hello world");
+// });
 server.listen(PORT, (err) => {
   if (err) console.log(err);
   console.log("Server running on Port ", PORT);
@@ -23,12 +37,29 @@ server.listen(PORT, (err) => {
 io.on("connection", (socket) => {
   console.log("client connected:", socket.id);
   socket.on("join-room", (data) => {
+    console.log(data);
     socket.join(data);
   });
   socket.on("send-message", (msg) => {
+    async function fetchAsync() {
+      let response = await fetch("http://localhost:8080/save/message", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          Authorization: "JWT " + msg.token,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(msg),
+      });
+      let data = await response.json();
+      console.log("this");
+      console.log(data);
+    }
+
     console.log("hi");
-    console.log(msg.room);
-    io.to(msg.room).emit("receive-message", msg);
+    console.log(msg.conversation_name);
+    io.to(msg.conversation_name).emit("receive-message", msg);
+    fetchAsync();
   });
 
   socket.on("disconnect", (reason) => {

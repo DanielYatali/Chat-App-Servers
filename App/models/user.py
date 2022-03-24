03e86faa59c94acc0f939971100de89f83e43d52
Daniel Yatali <danielyatali902@gmail.com>
2022-03-24 16:03:33 -0400

    added unique field to username

--- App/models/user.py
+++ App/models/user.py
@@ -1,9 +1,10 @@
+from enum import unique
 from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False)
+    username =  db.Column(db.String, nullable=False, unique = True)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 

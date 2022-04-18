13f948520d1fdc5935a48770f1ece6a3d7d442ad
Daniel Yatali <danielyatali902@gmail.com>
2022-04-18 17:03:01 -0400

    pain

--- App/views/conversation.py
+++ App/views/conversation.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory
 from flask_jwt import jwt_required, current_identity
-from flask_login import login_required
-import jwt
 
 from App.controllers import (
     create_conversation,

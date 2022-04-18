95e99021e75c3ac0466adbe390686a5079a188ab
Daniel Yatali <danielyatali902@gmail.com>
2022-04-18 18:32:04 -0400

    updated config

--- App/config.py
+++ App/config.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
-SECRET_KEY = "secret key"
+SECRET_KEY = "SECRET"
 JWT_EXPIRATION_DELTA = timedelta(days=7)
 # ENV = "DEVELOPMENT"
 ENV = "PRODUCTION"
